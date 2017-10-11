2131859cafc1503a9985b3798fc94faec2ac952c
codingo <codingo@protonmail.com>
2017-10-11 12:06:49 +1000

    Add version tracking to lib/core

---
+++ nosqlmap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# NoSQLMap Copyright 2012-2017 NoSQLMap Development team
+# See the file 'doc/COPYING' for copying permission
+
+import sys
+from lib.core.__version__ import __version__
+
+def print_banner():
+    print("╔╗╔┌─┐╔═╗╔═╗ ╦  ╔╦╗┌─┐┌─┐")
+    print("║║║│ │╚═╗║═╬╗║  ║║║├─┤├─┘")
+    print("╝╚╝└─┘╚═╝╚═╝╚╩═╝╩ ╩┴ ┴┴  ")
+    print(" Version %s www.nosqlmap.net" % __version__)
+    print(" By @tcstool and @codingo_")
+
+def main():
+    print_banner()
+
+
+
+
+if __name__ == '__main__':
+    main()
